ebf0c2aab53f20aa97e938ee1433c6c881423755
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-05-24 22:51:38 -0400

    beautify code a little bit

--- src/main.rs
+++ src/main.rs
@@ -7,70 +7,79 @@ use askama::Template;
 use std::time::{Duration, SystemTime};
 use std::option::Option;
 
+/// Defines a template with no values. This allows us to make use of the existing "template" infrastructure when
+/// serving static files.
 #[derive(Template)]
 #[template(path = "schedule.html")]
-struct ScheduleTemplate {
-}
+struct ScheduleTemplate;
 
+/// On the main path, we serve schedule.html
 #[get("/")]
 fn schedule() -> ScheduleTemplate {
     ScheduleTemplate {
     }
 }
 
+/// Defines a template for a given event.
 #[derive(Template)]
 #[template(path = "event.html")]
 struct EventTemplate {
     event_name: String,
+
+    /// This is the time of the event, exactly as the JS may format it. This is so that the user can go to the page,
+    /// and immediately see the time the event is taking place without having to wait for the JS to update it.
     friendly_time_string: String,
+
+    /// This is the time from the unix epoch that the event will take place. This is injected into the JS so that the
+    /// JS will update it every second.
     unix_epoch_time_offset: u64,
 }
 
-// apparently, even if i use Option<T> for the optional route, it doesn't work...?
-// so this is a weird workaround
+// note: the `event` route, even with `Option<String>`, doesn't work for an optional route without the name, so i made
+// another endpoint which will pass `Option::None` to the endpoint.
+// see: https://rocket.rs/v0.4/guide/requests/#optional-parameters
 #[get("/event?<time>")]
 fn event_without_name(time: u64) -> EventTemplate {
     event(Option::None, time)
 }
 
-// https://rocket.rs/v0.4/guide/requests/#optional-parameters
 #[get("/event/<name>?<time>")]
 fn event(name: Option<String>, time: u64) -> EventTemplate {
-    let mut name = match name {
-        Some(str) => str,
-        None => "Unnamed-Event".to_string(),
+    let name = match name {
+        Some(mut query_input) => {
+            mutate_url_input(&mut query_input);
+            query_input
+        },
+        None => "Unnamed Event".to_string(),
     };
 
-    // rewrite 'name' so that all - become spaces (and vice versa)
+    EventTemplate {
+        event_name: name,
+        friendly_time_string: get_time(time),
+        unix_epoch_time_offset: time
+    }
+}
+
+/// This mutates some query input into an event name.
+/// For SEO purposes, this turns all spaces into hyphens and vice versa.
+#[inline]
+fn mutate_url_input(query_input: &mut String) {
     unsafe {
-        // UTF-8 guarantees that all valid ASCII (spaces, dashes) are NOT part of multi byte sequences
-        // this is entirely safe and fast and allocation free.
-        for mut char in name.as_mut_vec() {
-            // dash
-            if *char == b'-' {
-                *char = b' ';
-            }
-            
-            // space
-            else if *char == b' ' {
-                *char = b'-';
-            }
+        for character in query_input.as_mut_vec() {
+            // this is perfectly legal and safe
+            // UTF-8 guarantees that no mutli character sequences have ASCII in them, so this is completely safe.
+            *character = match *character {
+                b' ' => b'-',
+                b'-' => b' ',
+                _ => *character,
+            };
         }
     }
-
-    let friendly_time_string = get_time(time);
-
-    let context = EventTemplate {
-        event_name: name,
-        friendly_time_string,
-        unix_epoch_time_offset: time
-    };
-
-    context
 }
 
 // TODO: rename/clean up these functions
 
+#[inline]
 fn get_time(event_time_ms: u64) -> String {
     let start_time = get_event_secs(event_time_ms);
     let mut time = if start_time < 0 { -start_time } else { start_time };
@@ -102,34 +111,40 @@ fn get_time(event_time_ms: u64) -> String {
     }
 }
 
+#[inline]
 fn get_event_secs(event_time_ms: u64) -> i128 {
+    println!("running get_event_secs for: {}", event_time_ms);
     let event_time = event_time_since_epoch(event_time_ms);
+    let now_time = SystemTime::now();
 
-    let time_until_event = event_time.duration_since(SystemTime::now());
-
-    if let Ok(duration) = time_until_event {
-        // event *is* in the future
-        return duration.as_secs() as i128;
-    } else {
-        // event was in the past
-        let time_since_event = SystemTime::now().duration_since(event_time);
-
-        match time_since_event {
-            Ok(duration) => {
-                return -(duration.as_secs() as i128);
-            },
-            _ => panic!("wtf")
-        };
+    // the event is in the past
+    if now_time > event_time {
+        let past = now_time.duration_since(event_time).unwrap();
+        -(past.as_secs() as i128)
+    }
+    else if now_time < event_time {
+        // event is in the future
+        let future = event_time.duration_since(now_time).unwrap();
+        future.as_secs() as i128
+    }
+    else {
+        // event and now are exactly the same. return 0
+        0
     }
 }
 
+/// This attemps to add the given time to the unix epoch to get a resulting time.
+#[inline]
 fn event_time_since_epoch(event_time_ms: u64) -> SystemTime {
-    let event_time = SystemTime::UNIX_EPOCH.checked_add(Duration::from_millis(event_time_ms));
+    let duration = Duration::from_millis(event_time_ms);
+    let event_time = SystemTime::UNIX_EPOCH.checked_add(duration);
 
-    if let None = event_time {
-        SystemTime::UNIX_EPOCH
-    } else {
-        event_time.unwrap()
+    match event_time {
+        // if we could add the time, we can return the time
+        Some(time) => time,
+
+        // if we couldn't add the time (unlikely to ever happen given that we're passing in a u64), return unix epoch
+        None => SystemTime::UNIX_EPOCH,
     }
 }
 
